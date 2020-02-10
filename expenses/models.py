9853f112d1be1c32bd14de260439393d8dcb1b07
Julian Nunez <julian.nunezm@gmail.com>
2020-02-10 23:59:07 +1100

    Create the Category and Expense model and register it on the admin module.

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(primary_key=True, max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Expense(models.Model):
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    description = models.TextField()
+
+# ToDo: Create Period and check what others Classes could be created. Budget?
