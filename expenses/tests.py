40c38791a75092bc762c4ff441d4425a260e3dde
Julian Nunez <julian.nunezm@gmail.com>
2020-02-11 00:58:10 +1100

    Create page to list Categories and the function to test it.

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,4 +1,6 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
+from .models import Category
 
 class SimpleTests(SimpleTestCase):
     def test_home_page_status_code(self):
@@ -8,3 +10,24 @@ class SimpleTests(SimpleTestCase):
     def test_about_page_status_code(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        Category.objects.create(name='Test', description='This is a description.')
+    
+    def test_categories_url_exists_at_proper_location(self):
+        response = self.client.get('/categories/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_categories_url_by_name(self):
+        response = self.client.get(reverse('categories'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_categories_url_uses_the_correct_template(self):
+        response = self.client.get(reverse('categories'))
+        self.assertTemplateUsed(response, 'categories.html')
+
+    def test_category_name(self):
+        category = Category.objects.get(description='This is a description.')
+        expected_object_name = f'{category.name}'
+        self.assertEqual(expected_object_name, 'Test')
--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Category
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class CategoryPageView(ListView):
+    model = Category
+    template_name = 'categories.html'
+    context_object_name = 'all_categories_list'
